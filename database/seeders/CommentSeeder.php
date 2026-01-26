58211d5fe8655f2d86ca3b65cfcd60d9683988d7
Alwaleed991 <adimfd2013@gmail.com>
2026-01-26 12:31:10 +0300

    implement factories and seeders for test data
    Added factories and database seeders to generate 5 users,2 posts for each user , and 3 comments on each post while paying attention for the relationships and data integrity.

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -4,14 +4,21 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\Comment;
 
 class CommentSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+     public function run(): void
     {
-        //
+        Post::all()->each(function ($post) {
+        Comment::factory(3)
+            ->for($post)
+            ->for($post->user)
+            ->create();
+    });
     }
 }
--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\User;
 
 class PostSeeder extends Seeder
 {
@@ -12,6 +14,10 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::all()->each(function ($user) {
+        Post::factory(2)
+            ->for($user) // user_id = $user->id  this is what happen behind the scenes
+            ->create();
+    });
     }
 }
