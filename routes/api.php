45102fb739ad7cafdce955ee4f1f1b45458f18e4
Alwaleed991 <adimfd2013@gmail.com>
2026-01-28 14:18:54 +0300

    Implement login with Sanctum and add auth tests

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\CommentController;
+use App\Http\Controllers\Api\V1\LoginController;
 use App\Http\Controllers\Api\V1\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,16 +10,22 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
 Route::prefix('v1')->group(function(){
-    Route::get('/posts',[PostController::class, 'index']);
-    Route::post('/posts',[PostController::class, 'store'])->middleware('auth:sanctum');
-    Route::get('/posts/{post}',[PostController::class, 'show']);
-    Route::patch('/posts/{post}',[PostController::class, 'update']);
-    Route::delete('/posts/{post}',[PostController::class, 'destroy']);
-    // Route::get('/posts/{post}/comments',[PostController::class, 'postComments'] );
-    Route::post('/comments',[CommentController::class, 'store']);
-    Route::patch('/comments/{comment}',[CommentController::class, 'update']);
-    Route::delete('/comments/{comment}',[CommentController::class, 'destroy']);
+    Route::post('/login', LoginController::class);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/posts',[PostController::class, 'index']);
+        Route::post('/posts',[PostController::class, 'store']);
+        Route::get('/posts/{post}',[PostController::class, 'show']);
+        Route::patch('/posts/{post}',[PostController::class, 'update']);
+        Route::delete('/posts/{post}',[PostController::class, 'destroy']);
+        // Route::get('/posts/{post}/comments',[PostController::class, 'postComments'] );
+        Route::post('/comments',[CommentController::class, 'store']);
+        Route::patch('/comments/{comment}',[CommentController::class, 'update']);
+        Route::delete('/comments/{comment}',[CommentController::class, 'destroy']);
+    });
+    
 });
 
 
