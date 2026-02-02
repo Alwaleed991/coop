403967b421500d3aba2adc5057971e208ed9bbf1
Alwaleed991 <adimfd2013@gmail.com>
2026-02-02 13:46:43 +0300

    add the test cases for the posts/store functionality

--- tests/Feature/Api/V1/PostTest.php
+++ tests/Feature/Api/V1/PostTest.php
@@ -35,3 +35,46 @@ it('does not allow user to list posts when not authenticated', function () {
     test()->getJson('/api/v1/posts')
     ->assertStatus(401);
 });
+
+it('allows user to store post successfully', function () {
+    $user = User::factory()->create();
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+
+    test()->withHeader('Authorization', 'bearer '.$token)->postJson('/api/v1/posts',[
+        "title"=>"post title",   
+        "body"=> "post body"
+    ])
+    ->assertStatus(201)
+    ->assertJsonStructure([
+        'message',
+        'data'=> [
+            'id', 'title', 'body','user_id'
+        ]
+    ]);
+});
+
+
+it('does not allows user to store post when not authenticated', function () {
+   
+    test()->postJson('/api/v1/posts',[
+        "title"=>"post title",   
+        "body"=> "post body"
+    ])
+    ->assertStatus(401);
+    
+});
+
+it('does not allows user to store post with empty credentials', function () {
+    $user = User::factory()->create();
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+
+    test()->withHeader('Authorization', 'bearer '.$token)->postJson('/api/v1/posts',[
+        "title"=>"",   
+        "body"=> ""
+    ])
+    ->assertStatus(422)
+     ->assertJsonValidationErrors(['title','body']);
+    
+});
