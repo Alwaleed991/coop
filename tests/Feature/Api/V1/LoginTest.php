7fa536b50482fb215e58ef06ed9e29788749241a
Alwaleed991 <adimfd2013@gmail.com>
2026-02-02 11:17:33 +0300

    add the test cases for the login functionality

---
+++ tests/Feature/Api/V1/LoginTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Models\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+uses(TestCase::class, RefreshDatabase::class);
+
+
+
+it('allows user to login and receive token', function () {
+    $user = User::factory()->create();
+
+    test()->postJson('/api/v1/login', [
+        'email' => $user->email,
+        'password' => 'password',
+    ])
+    ->assertOk()
+    ->assertJsonStructure([
+        'message',
+        'token',
+        'user',
+    ]);
+});
+
+
+it('not allows user to login with invalid credentials', function(){
+    $user = User::factory()->create();
+
+    test()->postJson('/api/v1/login', [
+        'email' => 'wrongEmail@gmail.com',
+        'password' => 'password',
+    ])
+    ->assertStatus(422)
+    ->assertJsonValidationErrors(['password']);
+
+});
+
+it('not allows user to login with empty credentials', function(){
+    $user = User::factory()->create();
+
+    test()->postJson('/api/v1/login', [
+        'email' => '',
+        'password' => '',
+    ])
+    ->assertStatus(422)
+    ->assertJsonValidationErrors(['email','password']);
+
+});
+
+
