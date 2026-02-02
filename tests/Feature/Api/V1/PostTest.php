29e95d70b460d90a7476870e2843ecffa3bc86c1
Alwaleed991 <adimfd2013@gmail.com>
2026-02-02 14:20:56 +0300

    add the test cases for the posts/show functionality

--- tests/Feature/Api/V1/PostTest.php
+++ tests/Feature/Api/V1/PostTest.php
@@ -78,3 +78,47 @@ it('does not allows user to store post with empty credentials', function () {
      ->assertJsonValidationErrors(['title','body']);
     
 });
+
+it('allows user to show single post', function () {
+    $user = User::factory()->create();
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    $posts = Post::factory(3)->create([
+        'user_id'=>$user->id
+    ]);
+
+    test()->withHeader('Authorization', 'bearer '.$token)->getJson('/api/v1/posts/'.$posts[1]->id)
+    ->assertStatus(200)
+    ->assertJsonStructure([
+        'data'=> [
+            'id', 'title', 'body','user_id'
+        ]
+    ]);
+    
+});
+
+it('does not allows user to show single post when not authenticated', function () {
+    $user = User::factory()->create();
+
+    $posts = Post::factory(3)->create([
+        'user_id'=>$user->id
+    ]);
+
+    test()->getJson('/api/v1/posts/'.$posts[1]->id)
+    ->assertStatus(401);
+    
+});
+
+it('does not allows user to show single post that does not exist', function () {
+    $user = User::factory()->create();
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    $posts = Post::factory(3)->create([
+        'user_id'=>$user->id
+    ]);
+
+    test()->withHeader('Authorization', 'bearer '.$token)->getJson('/api/v1/posts/X')
+    ->assertStatus(404);
+    
+});
+
